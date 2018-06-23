c8553d1c096c8ea161055e2d0cfd960df42fa986
Benjamin Hamon <hamon.benjamin@gmail.com>
2018-06-23 13:01:26 +0200

    Add generic data provider interface and implementation for json

---
+++ master/bhamon_build_master/data_provider.py
@@ -0,0 +1,23 @@
+import abc
+
+class DataProvider(abc.ABC):
+
+	@abc.abstractmethod
+	def get(self, key):
+		pass
+
+	@abc.abstractmethod
+	def get_all(self):
+		pass
+
+	@abc.abstractmethod
+	def create(self, key, data):
+		pass
+
+	@abc.abstractmethod
+	def update(self, key, data):
+		pass
+
+	@abc.abstractmethod
+	def delete(self, key):
+		pass
