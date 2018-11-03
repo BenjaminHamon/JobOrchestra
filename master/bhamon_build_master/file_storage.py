1789fc2b463fcd28c6c49b38fa86b486ff159fcd
Benjamin Hamon <hamon.benjamin@gmail.com>
2018-11-03 18:42:32 +0100

    Rename variables in file storage to suit its more generic purpose

--- master/bhamon_build_master/file_storage.py
+++ master/bhamon_build_master/file_storage.py
@@ -17,16 +17,16 @@ class FileStorage:
 		file_path = os.path.join(self._data_directory, file_path)
 		if not os.path.exists(file_path):
 			return ""
-		with open(file_path) as log_file:
-			return log_file.read()
+		with open(file_path) as data_file:
+			return data_file.read()
 
 
 	def save(self, file_path, data):
 		file_path = os.path.join(self._data_directory, file_path)
 		if not os.path.exists(os.path.dirname(file_path)):
 			os.makedirs(os.path.dirname(file_path))
-		with open(file_path + ".tmp", "w") as log_file:
-			log_file.write(data)
+		with open(file_path + ".tmp", "w") as data_file:
+			data_file.write(data)
 		if os.path.exists(file_path):
 			os.remove(file_path)
 		os.rename(file_path + ".tmp", file_path)
