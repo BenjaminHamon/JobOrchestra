21acfcb64b4918a3d350f51437c9cfea8a3ae3f0
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-01-27 17:25:44 +0100

    Prevent loading a page outside the valid range

--- website/bhamon_build_website/helpers.py
+++ website/bhamon_build_website/helpers.py
@@ -5,6 +5,6 @@ import flask
 
 def get_pagination(item_total):
 	item_count = max(min(flask.request.args.get("item_count", default = 100, type = int), 1000), 10)
-	page_number = max(flask.request.args.get("page", default = 1, type = int), 1)
 	page_total = int(math.ceil(item_total / item_count))
+	page_number = max(min(flask.request.args.get("page", default = 1, type = int), page_total), 1)
 	return { "page_number": page_number, "page_total": page_total, "item_count": item_count, "item_total": item_total}
