f4b2e88bb037a2d3a2d719b4efe9752c997800a2
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-02-09 16:50:59 +0100

    Add option to filter tests

--- scripts/commands/test.py
+++ scripts/commands/test.py
@@ -5,14 +5,16 @@ import uuid
 
 
 def configure_argument_parser(environment, configuration, subparsers): # pylint: disable=unused-argument
-	return subparsers.add_parser("test", help = "run the test suite")
+	parser = subparsers.add_parser("test", help = "run the test suite")
+	parser.add_argument("--filter", help = "specify a string expression to select tests to run")
+	return parser
 
 
 def run(environment, configuration, arguments): # pylint: disable=unused-argument
-	test(environment, arguments.simulate)
+	test(environment, arguments.filter, arguments.simulate)
 
 
-def test(environment, simulate):
+def test(environment, filter_expression, simulate):
 	run_identifier = uuid.uuid4()
 
 	logging.info("Running test suite (RunIdentifier: %s)", run_identifier)
@@ -23,6 +25,7 @@ def test(environment, simulate):
 	pytest_command = [ environment["python3_executable"], "-m", "pytest", "test", "--verbose" ]
 	pytest_command += [ "--collect-only" ] if simulate else []
 	pytest_command += [ "--basetemp", os.path.join("test_results", str(run_identifier)) ]
+	pytest_command += [ "-k", filter_expression ] if filter_expression else []
 
 	logging.info("+ %s", " ".join(pytest_command))
 	subprocess.check_call(pytest_command)
