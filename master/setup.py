dadbc804acdadbf8200f8af87b123fa28551f991
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-02-23 12:57:07 +0100

    Update setup scripts to retrieve information from the workspace configuration

--- master/setup.py
+++ master/setup.py
@@ -1,19 +1,37 @@
+import os
+import sys
+
 import setuptools
 
 
+setup_directory = os.path.dirname(os.path.realpath(__file__))
+workspace_directory = os.path.dirname(setup_directory)
+sys.path.insert(0, os.path.join(workspace_directory, "scripts"))
+
+
+import configuration # pylint: disable = import-error, wrong-import-position
+import environment # pylint: disable = import-error, wrong-import-position
+
+
+os.chdir(workspace_directory)
+
+component_name = "bhamon-build-master"
+environment_instance = environment.load_environment()
+configuration_instance = configuration.load_configuration(environment_instance)
+
+os.chdir(setup_directory)
+
+
+component = next(c for c in configuration_instance["components"] if c["name"] == component_name)
+
 setuptools.setup(
-	name = "bhamon-build-master",
-	version = "1.0",
-	description = "Master component for build service, responsible for supervising workers and builds",
-	author = "Benjamin Hamon",
-	author_email = "hamon.benjamin@gmail.com",
-	url = "https://github.com/BenjaminHamon/BuildService",
+	name = component["name"],
+	version = configuration_instance["project_version"]["full"],
+	description = component["description"],
+	author = configuration_instance["author"],
+	author_email = configuration_instance["author_email"],
+	url = configuration_instance["project_url"],
 
-	packages = [
-		"bhamon_build_master",
-	],
-
-	install_requires = [
-		"websockets",
-	],
+	packages = component["packages"],
+	install_requires = component.get("install_requires", []),
 )
--- worker/setup.py
+++ worker/setup.py
@@ -1,21 +1,37 @@
+import os
+import sys
+
 import setuptools
 
 
+setup_directory = os.path.dirname(os.path.realpath(__file__))
+workspace_directory = os.path.dirname(setup_directory)
+sys.path.insert(0, os.path.join(workspace_directory, "scripts"))
+
+
+import configuration # pylint: disable = import-error, wrong-import-position
+import environment # pylint: disable = import-error, wrong-import-position
+
+
+os.chdir(workspace_directory)
+
+component_name = "bhamon-build-worker"
+environment_instance = environment.load_environment()
+configuration_instance = configuration.load_configuration(environment_instance)
+
+os.chdir(setup_directory)
+
+
+component = next(c for c in configuration_instance["components"] if c["name"] == component_name)
+
 setuptools.setup(
-	name = "bhamon-build-worker",
-	version = "1.0",
-	description = "Worker component for build service, responsible for executing builds",
-	author = "Benjamin Hamon",
-	author_email = "hamon.benjamin@gmail.com",
-	url = "https://github.com/BenjaminHamon/BuildService",
+	name = component["name"],
+	version = configuration_instance["project_version"]["full"],
+	description = component["description"],
+	author = configuration_instance["author"],
+	author_email = configuration_instance["author_email"],
+	url = configuration_instance["project_url"],
 
-	packages = [
-		"bhamon_build_worker",
-	],
-
-	install_requires = [
-		"filelock",
-		"requests",
-		"websockets",
-	],
+	packages = component["packages"],
+	install_requires = component.get("install_requires", []),
 )
