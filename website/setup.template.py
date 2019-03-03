73dce8420629d0e68a5f3d7fcb39e3e29696cb08
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-03-03 15:23:35 +0100

    Include resources in website distribution package

--- website/setup.template.py
+++ website/setup.template.py
@@ -1,5 +1,17 @@
+import glob
+import os
+
 import setuptools
 
+
+def list_package_data(package, pattern_collection):
+	all_files = []
+	for pattern in pattern_collection:
+		all_files += glob.glob(package + "/" + pattern, recursive = True)
+	return [ os.path.relpath(path, package) for path in all_files ]
+
+
+resource_patterns = [ 'static/**/*.css', 'static/**/*.js', 'templates/**/*.html' ]
 
 setuptools.setup(
 	name = "bhamon-build-website",
@@ -10,6 +22,7 @@ setuptools.setup(
 	url = "{url}",
 
 	packages = [ "bhamon_build_website" ],
+	package_data = { "bhamon_build_website": list_package_data("bhamon_build_website", resource_patterns) },
 
 	install_requires = [ "flask", "requests" ],
 )
