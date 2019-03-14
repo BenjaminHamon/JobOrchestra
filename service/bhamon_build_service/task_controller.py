015cf64eef0e537a07d3f85d3ed0dcf09aa92d33
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-03-14 21:00:25 +0100

    Fix service method for cancelling tasks

--- service/bhamon_build_service/task_controller.py
+++ service/bhamon_build_service/task_controller.py
@@ -38,5 +38,5 @@ def get_task(task_identifier):
 def cancel_task(task_identifier):
 	task = flask.current_app.task_provider.get(task_identifier)
 	if task["status"] == "pending":
-		flask.current_app.task_provider.update(task, should_cancel = True)
+		flask.current_app.task_provider.update_status(task, should_cancel = True)
 	return flask.jsonify({})
