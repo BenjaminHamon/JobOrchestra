8e5b303eb89fee9c9545a7786a12c867d55f58c4
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-06-30 21:52:35 +0200

    Refactor develop command

--- scripts/commands/develop.py
+++ scripts/commands/develop.py
@@ -13,27 +13,29 @@ def configure_argument_parser(environment, configuration, subparsers): # pylint:
 
 
 def run(environment, configuration, arguments): # pylint: disable=unused-argument
+	install_dependencies(environment["python3_executable"], configuration["development_dependencies"], arguments.simulate)
+	print("")
 	for component in configuration["components"]:
 		scripts.commands.distribute.setup(configuration, component, arguments.simulate)
 	print("")
-	install(environment["python3_executable"], configuration["development_dependencies"], configuration["components"], arguments.simulate)
-	print("")
+	for component in configuration["components"]:
+		install_component(environment["python3_executable"], component, arguments.simulate)
+		print("")
 
 
-def install(python_executable, dependency_collection, component_collection, simulate):
+def install_dependencies(python_executable, dependency_collection, simulate):
 	logger.info("Installing development dependencies")
 
 	install_command = [ python_executable, "-m", "pip", "install", "--upgrade" ] + dependency_collection
 	logger.info("+ %s", " ".join(install_command))
 	if not simulate:
 		subprocess.check_call(install_command)
-		print("")
 
-	for component in component_collection:
-		logger.info("Installing development package for '%s'", component["name"])
 
-		install_command = [ python_executable, "-m", "pip", "install", "--upgrade", "--editable", os.path.join(".", component["path"]) ]
-		logger.info("+ %s", " ".join(install_command))
-		if not simulate:
-			subprocess.check_call(install_command)
-			print("")
+def install_component(python_executable, component, simulate):
+	logger.info("Installing development package for '%s'", component["name"])
+
+	install_command = [ python_executable, "-m", "pip", "install", "--upgrade", "--editable", os.path.join(".", component["path"]) ]
+	logger.info("+ %s", " ".join(install_command))
+	if not simulate:
+		subprocess.check_call(install_command)
