c31f38bf14af35f98b18ab8c2d0d8fdc2fc30401
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-08-10 23:18:50 +0200

    Separate linting for packages and for tests

--- scripts/commands/lint.py
+++ scripts/commands/lint.py
@@ -11,14 +11,24 @@ def configure_argument_parser(environment, configuration, subparsers): # pylint:
 
 
 def run(environment, configuration, arguments): # pylint: disable = unused-argument
-	lint(environment["python3_executable"], configuration["components"])
+	lint_packages(environment["python3_executable"], configuration["components"])
+	lint_tests(environment["python3_executable"], "test")
 
 
-def lint(python_executable, component_collection):
-	logger.info("Running linter")
+def lint_packages(python_executable, component_collection):
+	logger.info("Running linter in python packages")
 
 	pylint_command = [ python_executable, "-m", "pylint" ]
-	pylint_command += [ os.path.join(component["path"], component["packages"][0]) for component in component_collection ] + [ "test" ]
+	pylint_command += [ os.path.join(component["path"], component["packages"][0]) for component in component_collection ]
+
+	logger.info("+ %s", " ".join(pylint_command))
+	subprocess.check_call(pylint_command)
+
+
+def lint_tests(python_executable, test_directory):
+	logger.info("Running linter in python tests")
+
+	pylint_command = [ python_executable, "-m", "pylint", test_directory ]
 
 	logger.info("+ %s", " ".join(pylint_command))
 	subprocess.check_call(pylint_command)
