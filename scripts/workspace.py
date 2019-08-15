dc5a40883da36b7756eb7ccfa70b039c17bec6e9
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-08-15 22:02:19 +0200

    Use a intermediate file when saving command results

--- scripts/workspace.py
+++ scripts/workspace.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 
 
 def load_results(result_file_path):
@@ -15,8 +16,11 @@ def load_results(result_file_path):
 	return results
 
 
-def save_results(result_file_path, result_data):
+def save_results(result_file_path, results):
 	if os.path.dirname(result_file_path):
 		os.makedirs(os.path.dirname(result_file_path), exist_ok = True)
-	with open(result_file_path, "w") as result_file:
-		json.dump(result_data, result_file, indent = 4)
+	with open(result_file_path + ".tmp", "w") as result_file:
+		json.dump(results, result_file, indent = 4)
+	if os.path.isfile(result_file_path):
+		os.remove(result_file_path)
+	shutil.move(result_file_path + ".tmp", result_file_path)
