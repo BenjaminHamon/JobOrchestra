7970035c7ad5ec402112429f1ec073a1ee4fdd54
Benjamin Hamon <hamon.benjamin@gmail.com>
2019-09-07 19:02:18 +0200

    Refactor task processor and add tests

--- master/bhamon_build_master/task_processor.py
+++ master/bhamon_build_master/task_processor.py
@@ -43,23 +43,11 @@ class TaskProcessor:
 
 	async def _update(self):
 		try:
-			all_tasks = self._task_provider.get_list(status = "pending")
-			all_tasks.sort(key = self._get_task_order)
+			all_tasks = self._list_pending_tasks()
 
 			for task in all_tasks:
-				logger.debug("Processing task %s (Type: %s, Parameters: %s)", task["identifier"], task["type"], task["parameters"])
-
 				try:
-					self._task_provider.update_status(task, status = "running")
-					if task["should_cancel"]:
-						cancellation_handler = self._handler_collection[task["type"]]["cancellation_handler"]
-						if cancellation_handler:
-							cancellation_handler(task["parameters"])
-						end_status = "cancelled"
-					else:
-						end_status = self._handler_collection[task["type"]]["execution_handler"](task["parameters"])
-					self._task_provider.update_status(task, status = end_status)
-
+					self._execute_task(task)
 				except Exception: # pylint: disable = broad-except
 					logger.error("Unhandled exception while processing task %s", task["identifier"], exc_info = True)
 					self._task_provider.update_status(task, status = "exception")
@@ -68,14 +56,32 @@ class TaskProcessor:
 			logger.error("Unhandled exception", exc_info = True)
 
 
+	def _list_pending_tasks(self):
+
+		def get_task_order(task):
+			task_handler = self._handler_collection.get(task["type"], { "order": 99999 })
+			return (task_handler["order"], task["creation_date"])
+
+		all_tasks = self._task_provider.get_list(status = "pending")
+		all_tasks.sort(key = get_task_order)
+		return all_tasks
+
+
+	def _execute_task(self, task):
+		logger.debug("Executing task %s (Type: %s, Parameters: %s)", task["identifier"], task["type"], task["parameters"])
+
+		self._task_provider.update_status(task, status = "running")
+		if task["should_cancel"]:
+			cancellation_handler = self._handler_collection[task["type"]]["cancellation_handler"]
+			if cancellation_handler is not None:
+				cancellation_handler(task["parameters"])
+			end_status = "cancelled"
+		else:
+			end_status = self._handler_collection[task["type"]]["execution_handler"](task["parameters"])
+		self._task_provider.update_status(task, status = end_status)
+
+
 	def shutdown(self):
 		self._should_shutdown = True
 		if self._active_asyncio_sleep:
 			self._active_asyncio_sleep.cancel()
-
-
-	def _get_task_order(self, task):
-		try:
-			return (self._handler_collection[task["type"]]["order"], task["creation_date"])
-		except KeyError:
-			return (99999, task["creation_date"])
