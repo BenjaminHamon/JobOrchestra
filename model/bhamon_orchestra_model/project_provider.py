77019cda4acac2f588b702dc6c745e60c603d3ad
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-01-10 22:29:52 +0100

    Add project provider

---
+++ model/bhamon_orchestra_model/project_provider.py
@@ -0,0 +1,48 @@
+import datetime
+import logging
+
+
+logger = logging.getLogger("ProjectProvider")
+
+
+class ProjectProvider:
+
+
+	def __init__(self, database_client):
+		self.database_client = database_client
+		self.table = "project"
+
+
+	def count(self):
+		return self.database_client.count(self.table, {})
+
+
+	def get_list(self, skip = 0, limit = None, order_by = None):
+		return self.database_client.find_many(self.table, {}, skip = skip, limit = limit, order_by = order_by)
+
+
+	def get(self, project_identifier):
+		return self.database_client.find_one(self.table, { "identifier": project_identifier })
+
+
+	def create_or_update(self, project_identifier):
+		project = self.get(project_identifier)
+
+		if project is None:
+			project = {
+				"identifier": project_identifier,
+				"creation_date": datetime.datetime.utcnow().replace(microsecond = 0).isoformat() + "Z",
+				"update_date": datetime.datetime.utcnow().replace(microsecond = 0).isoformat() + "Z",
+			}
+
+			self.database_client.insert_one(self.table, project)
+
+		else:
+			update_data = {
+				"update_date": datetime.datetime.utcnow().replace(microsecond = 0).isoformat() + "Z",
+			}
+
+			project.update(update_data)
+			self.database_client.update_one(self.table, { "identifier": project_identifier }, update_data)
+
+		return project
