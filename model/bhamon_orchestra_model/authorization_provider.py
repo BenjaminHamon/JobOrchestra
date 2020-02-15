87f7e9df081097d75d904f4e662d9dca17b2e487
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-02-15 16:56:06 +0100

    Add documentation and type hints to AuthorizationProvider

--- model/bhamon_orchestra_model/authorization_provider.py
+++ model/bhamon_orchestra_model/authorization_provider.py
@@ -1,41 +1,60 @@
 import logging
 
+from typing import List
+
 
 logger = logging.getLogger("AuthorizationProvider")
 
 
 class AuthorizationProvider:
+	""" Expose methods to check if a user is authorized to view a resource or perform an action.
+
+	Authorization is computed based on the user (anonymous or not, enabled or not, their roles)
+	and the resource or action they target (defined by an identifier or a route).
+
+	Roles:
+	- Administrator: full read-write access to web routes
+	- Auditor: full read-only access to web routes
+	- Operator: read-write access to web routes by whitelist (all but administration)
+	- Viewer: read-only access to web routes by whitelist (all but administration)
+	- Worker: connection access to the master, access to web routes for triggering jobs and viewing runs
+	"""
 
 
-	def authorize_request(self, user, method, route):
+	def authorize_request(self, user: dict, method: str, route: str) -> bool:
+		""" Check if a user is authorized to perform a web request with the provided method and route """
 		if self.is_public_route(method, route):
 			return True
-		if self.is_authorized_user(user) and self.is_authorized_route(user["roles"], method, route):
+		if self.is_active_user(user) and self.is_route_authorized_for_roles(user["roles"], method, route):
 			return True
 		return False
 
 
-	def authorize_view(self, user, view):
-		if self.is_authorized_user(user) and self.is_authorized_view(user["roles"], view):
+	def authorize_view(self, user: dict, view: str) -> bool:
+		""" Check if a user is authorized to see a set of resources, based on a view identifier
+
+		This is useful when displaying a complex web page with links to resources with varying authorization requirements. """
+
+		if self.is_active_user(user) and self.is_view_authorized_for_roles(user["roles"], view):
 			return True
 		return False
 
 
-	def authorize_worker(self, user):
-		if self.is_authorized_user(user) and "Worker" in user["roles"]:
+	def authorize_worker(self, user: dict) -> bool:
+		""" Check if a user is authorized to run a worker """
+		if self.is_active_user(user) and "Worker" in user["roles"]:
 			return True
 		return False
 
 
-	def get_administrator_roles(self): # pylint: disable = no-self-use
-		return [ "Administrator" ]
-
-
-	def is_authorized_user(self, user): # pylint: disable = no-self-use
+	def is_active_user(self, user: dict) -> bool: # pylint: disable = no-self-use
+		""" Check if a user is registered and enabled """
 		return user is not None and user["is_enabled"]
 
 
-	def is_public_route(self, method, route): # pylint: disable = no-self-use, too-many-return-statements
+	def is_public_route(self, method: str, route: str) -> bool: # pylint: disable = no-self-use, too-many-return-statements
+		""" Check if a route is public, meaning any user, even anonymous, is authorized to use it """
+
 		if method == "GET" and route == "/":
 			return True
 		if method == "GET" and route == "/help":
@@ -55,7 +74,9 @@ class AuthorizationProvider:
 		return False
 
 
-	def is_authorized_route(self, user_roles, method, route): # pylint: disable = too-many-branches, too-many-return-statements
+	def is_route_authorized_for_roles(self, user_roles: List[str], method: str, route: str) -> bool: # pylint: disable = too-many-branches, too-many-return-statements
+		""" Check if the given set of user roles authorizes using the provided method and route for a web request """
+
 		if "Administrator" in user_roles:
 			return True
 		if "Auditor" in user_roles and method == "GET":
@@ -94,7 +115,9 @@ class AuthorizationProvider:
 		return False
 
 
-	def is_authorized_view(self, user_roles, view): # pylint: disable = no-self-use, too-many-return-statements
+	def is_view_authorized_for_roles(self, user_roles: List[str], view: str) -> bool: # pylint: disable = no-self-use, too-many-return-statements
+		""" Check if the given set of user roles authorizes seeing a set of resources, based on a view identifier """
+
 		if "Administrator" in user_roles:
 			return True
 
@@ -116,5 +139,11 @@ class AuthorizationProvider:
 		return False
 
 
-	def get_route_domain(self, route): # pylint: disable = no-self-use
+	def get_administrator_roles(self) -> List[str]: # pylint: disable = no-self-use
+		""" Return the default list of roles for a user with administrator privileges """
+		return [ "Administrator" ]
+
+
+	def get_route_domain(self, route: str) -> str: # pylint: disable = no-self-use
+		""" Return the domain from a route, meaning its first element """
 		return route.replace("_", "/").split("/")[1]
