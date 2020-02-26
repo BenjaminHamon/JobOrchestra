580003666314206d0cc0fff8eb06a0374cbe040d
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-02-26 21:23:26 +0100

    Order jobs and schedules by identifier rather than update date

--- website/bhamon_orchestra_website/project_controller.py
+++ website/bhamon_orchestra_website/project_controller.py
@@ -26,9 +26,9 @@ def show_collection():
 def show(project_identifier): # pylint: disable = unused-argument
 	view_data = {
 		"project": service_client.get("/project/{project_identifier}".format(**locals())),
-		"job_collection": service_client.get("/project/{project_identifier}/job_collection".format(**locals()), { "limit": 10, "order_by": [ "update_date descending" ] }),
+		"job_collection": service_client.get("/project/{project_identifier}/job_collection".format(**locals()), { "limit": 10, "order_by": [ "identifier ascending" ] }),
 		"run_collection": service_client.get("/project/{project_identifier}/run_collection".format(**locals()), { "limit": 10, "order_by": [ "update_date descending" ] }),
-		"schedule_collection": service_client.get("/project/{project_identifier}/schedule_collection".format(**locals()), { "limit": 10, "order_by": [ "update_date descending" ] }),
+		"schedule_collection": service_client.get("/project/{project_identifier}/schedule_collection".format(**locals()), { "limit": 10, "order_by": [ "identifier ascending" ] }),
 		"worker_collection": service_client.get("/worker_collection", { "limit": 1000, "order_by": [ "identifier ascending" ] }),
 	}
 
