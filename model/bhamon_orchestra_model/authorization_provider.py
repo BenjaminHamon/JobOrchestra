aa6af7d591e154797c7e7250861f34ec4f2f0f40
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-02-29 13:27:56 +0100

    Extract authorization checks into classes for each user role
    Managing authorization in a single class with unclear method names was becoming cumbersome, especially since the resource structure is more complex due to projects.
    Instead of going through various methods in the authorization provider, the authorization methods now instantiate user roles classes and check that at least one gives access.
    This way it is simpler to add or extend user roles and the provider interface is smaller. Anonymous and default roles are also no longer special cases (except for being implicit).
    The downside is moving authorizations around, making comparisons between roles harder, which would have been the case anyway when adding more routes.

--- model/bhamon_orchestra_model/authorization_provider.py
+++ model/bhamon_orchestra_model/authorization_provider.py
@@ -1,149 +1,70 @@
 import logging
 
-from typing import List
+from typing import List, Optional
+
+import bhamon_orchestra_model.user_roles as user_role_classes
 
 
 logger = logging.getLogger("AuthorizationProvider")
 
 
 class AuthorizationProvider:
-	""" Expose methods to check if a user is authorized to view a resource or perform an action.
-
-	Authorization is computed based on the user (anonymous or not, enabled or not, their roles)
-	and the resource or action they target (defined by an identifier or a route).
-
-	Roles:
-	- Administrator: full read-write access to web routes
-	- Auditor: full read-only access to web routes
-	- Operator: read-write access to web routes by whitelist (all but administration)
-	- Viewer: read-only access to web routes by whitelist (all but administration)
-	- Worker: connection access to the master, access to web routes for triggering jobs and viewing runs
-	"""
+	""" Expose methods to check if a user is authorized to view a resource or perform an action. """
 
 
-	def authorize_request(self, user: dict, method: str, route: str) -> bool:
+	def authorize_request(self, user: Optional[dict], method: str, route: str) -> bool:
 		""" Check if a user is authorized to perform a web request with the provided method and route """
-		if self.is_public_route(method, route):
-			return True
-		if self.is_active_user(user) and self.is_route_authorized_for_roles(user["roles"], method, route):
-			return True
+
+		user_roles = self.build_user_roles(user)
+
+		for role in user_roles:
+			if role.is_route_authorized(method, route):
+				return True
+
 		return False
 
 
-	def authorize_view(self, user: dict, view: str) -> bool:
+	def authorize_view(self, user: Optional[dict], view: str) -> bool:
 		""" Check if a user is authorized to see a set of resources, based on a view identifier
 
 		This is useful when displaying a complex web page with links to resources with varying authorization requirements. """
 
-		if self.is_active_user(user) and self.is_view_authorized_for_roles(user["roles"], view):
-			return True
+		user_roles = self.build_user_roles(user)
+
+		for role in user_roles:
+			if role.is_view_authorized(view):
+				return True
+
 		return False
 
 
-	def authorize_worker(self, user: dict) -> bool:
+	def authorize_worker(self, user: Optional[dict]) -> bool: # pylint: disable = no-self-use
 		""" Check if a user is authorized to run a worker """
-		if self.is_active_user(user) and "Worker" in user["roles"]:
-			return True
-		return False
+		return user is not None and user["is_enabled"] and "Worker" in user["roles"]
 
 
-	def is_active_user(self, user: dict) -> bool: # pylint: disable = no-self-use
-		""" Check if a user is registered and enabled """
-		return user is not None and user["is_enabled"]
+	def build_user_roles(self, user: Optional[dict]) -> List[object]: # pylint: disable = no-self-use
+		""" Instantiate user role classes based on a user record """
 
+		if user is None or not user["is_enabled"]:
+			return [ user_role_classes.Anonymous() ]
 
-	def is_public_route(self, method: str, route: str) -> bool: # pylint: disable = no-self-use, too-many-return-statements
-		""" Check if a route is public, meaning any user, even anonymous, is authorized to use it """
+		user_roles = [ user_role_classes.Default() ]
 
-		if method == "GET" and route == "/":
-			return True
-		if method == "GET" and route == "/help":
-			return True
-		if method == "GET" and route in [ "/me/login", "/me/logout" ]:
-			return True
-		if method == "POST" and route in [ "/me/login", "/me/logout" ]:
-			return True
-		if method == "GET" and route == "/service_proxy":
-			return True
-		if method == "GET" and route.startswith("/service_proxy/"):
-			return True
-		if method == "POST" and route.startswith("/service_proxy/"):
-			return True
-		if method == "GET" and route.startswith("/static/"):
-			return True
-		return False
+		if "Administrator" in user["roles"]:
+			user_roles.append(user_role_classes.Administrator())
+		if "Auditor" in user["roles"]:
+			user_roles.append(user_role_classes.Auditor())
+		if "Operator" in user["roles"]:
+			user_roles.append(user_role_classes.Operator())
+		if "Viewer" in user["roles"]:
+			user_roles.append(user_role_classes.Viewer())
+		if "Worker" in user["roles"]:
+			user_roles.append(user_role_classes.Worker())
 
-
-	def is_route_authorized_for_roles(self, user_roles: List[str], method: str, route: str) -> bool: # pylint: disable = too-many-branches, too-many-return-statements
-		""" Check if the given set of user roles authorizes using the provided method and route for a web request """
-
-		if "Administrator" in user_roles:
-			return True
-		if "Auditor" in user_roles and method == "GET":
-			return True
-
-		if method == "GET" and route == "/":
-			return True
-		if method == "GET" and route == "/help":
-			return True
-
-		domain = self.get_route_domain(route)
-
-		if domain == "me":
-			return True
-
-		if domain in [ "admin", "user" ]:
-			if "Administrator" in user_roles:
-				return True
-			if "Auditor" in user_roles and method == "GET":
-				return True
-
-		if method == "GET" and domain in [ "project", "worker" ]:
-			if "Viewer" in user_roles:
-				return True
-		if method == "POST" and domain in [ "project", "worker" ]:
-			if "Operator" in user_roles:
-				return True
-
-		if method == "GET" and route == "/project/<project_identifier>/run/<run_identifier>":
-			if "Worker" in user_roles:
-				return True
-		if method == "POST" and route == "/project/<project_identifier>/job/<job_identifier>/trigger":
-			if "Worker" in user_roles:
-				return True
-
-		return False
-
-
-	def is_view_authorized_for_roles(self, user_roles: List[str], view: str) -> bool: # pylint: disable = no-self-use, too-many-return-statements
-		""" Check if the given set of user roles authorizes seeing a set of resources, based on a view identifier """
-
-		if "Administrator" in user_roles:
-			return True
-
-		if view in [ "nav-admin" ]:
-			if "Administrator" in user_roles:
-				return True
-			if "Auditor" in user_roles:
-				return True
-		if view in [ "nav-main" ]:
-			if "Auditor" in user_roles:
-				return True
-			if "Viewer" in user_roles:
-				return True
-
-		if view in [ "job-actions", "run-actions", "schedule-actions", "worker-actions" ]:
-			if "Operator" in user_roles:
-				return True
-
-		return False
+		return user_roles
 
 
 	def get_administrator_roles(self) -> List[str]: # pylint: disable = no-self-use
 		""" Return the default list of roles for a user with administrator privileges """
 		return [ "Administrator" ]
-
-
-	def get_route_domain(self, route: str) -> str: # pylint: disable = no-self-use
-		""" Return the domain from a route, meaning its first element """
-		return route.replace("_", "/").split("/")[1]
---
+++ model/bhamon_orchestra_model/user_roles.py
@@ -0,0 +1,143 @@
+""" Implementation of authorization checks for all user roles, used by AuthorizationProvider """
+
+# pylint: disable = no-self-use, unused-argument
+
+
+class Anonymous:
+	""" Automatic role for a user not logged in, with minimal access to web routes (home, help, login, static) """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		if method == "GET":
+			if route in [ "/", "/help" ]:
+				return True
+			if route.startswith("/static/"):
+				return True
+
+		if method in [ "GET", "POST" ]:
+			if route in [ "/me/login", "/me/logout" ]:
+				return True
+
+		return False
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		return False
+
+
+
+class Default:
+	""" Automatic role for any user logged in, with minimal access to web routes (home, help, me, service proxy, static) """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		if method == "GET":
+			if route in [ "/", "/help", "/me", "/service_proxy" ]:
+				return True
+			if route.startswith("/static/"):
+				return True
+
+		if method in [ "GET", "POST" ]:
+			if route.startswith("/me/"):
+				return True
+			if route.startswith("/service_proxy/"):
+				return True
+
+		return False
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		return False
+
+
+
+class Administrator:
+	""" User role with full read-write access to web routes """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		return True
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		return True
+
+
+
+class Auditor:
+	""" User role with full read access to web routes """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		return method == "GET"
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		return True
+
+
+
+class Operator:
+	""" User role with write access to web routes by whitelist (all but administration) """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		if method == "POST":
+			if route.startswith("/project/<project_identifier>/job/"):
+				return True
+			if route.startswith("/project/<project_identifier>/run/"):
+				return True
+			if route.startswith("/project/<project_identifier>/schedule/"):
+				return True
+			if route.startswith("/worker/"):
+				return True
+
+		return False
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		if view in [ "job-actions", "run-actions", "schedule-actions", "worker-actions" ]:
+			return True
+		return False
+
+
+
+class Viewer:
+	""" User role with read access to web routes by whitelist (all but administration) """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		if method == "GET":
+			if route in [ "/project_collection", "/project_count" ]:
+				return True
+			if route.startswith("/project/"):
+				return True
+			if route in [ "/worker_collection", "/worker_count" ]:
+				return True
+			if route.startswith("/worker/"):
+				return True
+
+		return False
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		if view in [ "nav-main" ]:
+			return True
+		return False
+
+
+
+class Worker:
+	""" User role with connection access to the master, access to web routes for triggering jobs and viewing runs """
+
+
+	def is_route_authorized(self, method: str, route: str) -> bool:
+		if method == "GET" and route == "/project/<project_identifier>/run/<run_identifier>":
+			return True
+		if method == "POST" and route == "/project/<project_identifier>/job/<job_identifier>/trigger":
+			return True
+		return False
+
+
+	def is_view_authorized(self, view: str) -> bool:
+		return False
