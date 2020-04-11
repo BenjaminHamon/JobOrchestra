132b74a46c1e0742f61d0d8c3b314fcae5af1751
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-04-11 11:55:25 +0200

    Rework main development scripts and logging configuration

--- development/check.py
+++ development/check.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import shutil
 import sys
 
 sys.path.insert(0, os.path.join(sys.path[0], ".."))
@@ -12,26 +13,35 @@ logger = logging.getLogger("Main")
 
 
 def main():
-	current_directory = os.getcwd()
-	script_path = os.path.realpath(__file__)
-	workspace_directory = os.path.dirname(os.path.dirname(script_path))
+	with development.environment.execute_in_workspace(__file__):
+		environment_instance = development.environment.load_environment()
+		configuration_instance = development.configuration.load_configuration(environment_instance)
+		development.environment.configure_logging(environment_instance, None)
 
-	os.chdir(workspace_directory)
+		global_status = { "success": True }
 
-	try:
-		development.environment.configure_logging(logging.INFO)
-		command_list = development.configuration.load_commands()
-		check_commands(command_list)
+		check_commands(global_status)
+		check_software(global_status, environment_instance)
 
-	finally:
-		os.chdir(current_directory)
+	if not global_status["success"]:
+		raise RuntimeError("Check found issues")
 
 
-def check_commands(command_list):
+def check_commands(global_status):
+	command_list = development.configuration.load_commands()
+
 	for command in command_list:
 		if "exception" in command:
+			global_status["success"] = False
 			logger.error("Command '%s' is unavailable", command["module_name"], exc_info = command["exception"])
 			print("")
+
+
+def check_software(global_status, environment_instance):
+	python3_system_executable = environment_instance.get("python3_system_executable", None)
+	if python3_system_executable is None or not shutil.which(python3_system_executable):
+		global_status["success"] = False
+		logger.error("Python3 is required (Path: '%s')", environment_instance["python3_system_executable"])
 
 
 if __name__ == "__main__":
--- development/main.py
+++ development/main.py
@@ -18,39 +18,32 @@ def main():
 	if "__PYVENV_LAUNCHER__" in os.environ:
 		del os.environ["__PYVENV_LAUNCHER__"]
 
-	current_directory = os.getcwd()
-	script_path = os.path.abspath(__file__)
-	workspace_directory = os.path.dirname(os.path.dirname(script_path))
-
-	os.chdir(workspace_directory)
-
-	try:
+	with development.environment.execute_in_workspace(__file__):
 		environment_instance = development.environment.load_environment()
 		configuration_instance = development.configuration.load_configuration(environment_instance)
 		command_list = development.configuration.load_commands()
 
 		arguments = parse_arguments(environment_instance, configuration_instance, command_list)
-		log_level = logging.getLevelName(arguments.verbosity.upper())
-		development.environment.configure_logging(log_level)
-		if arguments.log_file is not None:
-			development.environment.configure_log_file(log_level, arguments.log_file)
+		development.environment.configure_logging(environment_instance, arguments)
 
 		show_project_information(configuration_instance, arguments.simulate)
 		arguments.func(environment_instance, configuration_instance, arguments)
 
-	finally:
-		os.chdir(current_directory)
-
 
 def parse_arguments(environment_instance, configuration_instance, command_list):
-	all_log_levels = [ "debug", "info", "warning", "error", "critical" ]
+	all_log_levels = development.environment.all_log_levels
 
 	main_parser = argparse.ArgumentParser()
-	main_parser.add_argument("--verbosity", choices = all_log_levels, default = "info",
+	main_parser.add_argument("--simulate", action = "store_true",
+			help = "perform a test run, without writing changes")
+	main_parser.add_argument("--verbosity", choices = all_log_levels,
 			metavar = "<level>", help = "set the logging level (%s)" % ", ".join(all_log_levels))
-	main_parser.add_argument("--simulate", action = "store_true", help = "perform a test run, without writing changes")
-	main_parser.add_argument("--log-file", metavar = "<file_path>", help = "set the log file path")
-	main_parser.add_argument("--results", metavar = "<file_path>", help = "set the file path where to store command results")
+	main_parser.add_argument("--log-file",
+			metavar = "<file_path>", help = "set the log file path")
+	main_parser.add_argument("--log-file-verbosity", choices = all_log_levels,
+			metavar = "<level>", help = "set the logging level for the log file (%s)" % ", ".join(all_log_levels))
+	main_parser.add_argument("--results",
+			metavar = "<file_path>", help = "set the file path where to store command results")
 
 	subparsers = main_parser.add_subparsers(title = "commands", metavar = "<command>")
 	subparsers.required = True
@@ -63,7 +56,7 @@ def parse_arguments(environment_instance, configuration_instance, command_list):
 
 def show_project_information(configuration_instance, simulate):
 	logger.info("%s %s", configuration_instance["project_name"], configuration_instance["project_version"]["full"])
-	logger.info("Script executing in %s %s", os.getcwd(), "(simulation)" if simulate else '')
+	logger.info("Script executing in '%s' %s", os.getcwd(), "(simulation)" if simulate else '')
 	print("")
 
 
