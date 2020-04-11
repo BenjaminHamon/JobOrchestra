c0682947a835c4a57aa9417ebe8cb057f29eccfd
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-04-11 11:35:16 +0200

    Add info command

--- development/configuration.py
+++ development/configuration.py
@@ -108,6 +108,7 @@ def load_commands():
 		"development.commands.clean",
 		"development.commands.develop",
 		"development.commands.distribute",
+		"development.commands.info",
 		"development.commands.lint",
 		"development.commands.release",
 		"development.commands.test_distribution",
