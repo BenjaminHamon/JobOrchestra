c05364b44a7ad1dbe983d6fb7be4b6f76fefa14d
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-08-27 21:18:02 +0200

    Implement model and client for SQL databases

---
+++ model/bhamon_orchestra_model/database/sql_database_administration.py
@@ -0,0 +1,45 @@
+import logging
+
+from typing import List, Tuple
+
+from sqlalchemy.engine import Connection
+from sqlalchemy.schema import MetaData
+
+
+logger = logging.getLogger("SqlDatabaseAdministration")
+
+
+class SqlDatabaseAdministration:
+	""" Administration client for a SQL database. """
+
+
+	def __init__(self, connection: Connection, metadata: MetaData) -> None:
+		self.connection = connection
+		self.metadata = metadata
+
+
+	def __enter__(self):
+		return self
+
+
+	def __exit__(self, exception_type, exception_value, traceback):
+		self.close()
+
+
+	def initialize(self, simulate: bool = False) -> None:
+		logger.info("Initializing" + (" (simulation)" if simulate else "")) # pylint: disable = logging-not-lazy
+
+		if not simulate:
+			self.metadata.create_all(self.connection.engine)
+
+
+	def upgrade(self, simulate: bool = False) -> None:
+		raise NotImplementedError()
+
+
+	def create_index(self, table: str, identifier: str, field_collection: List[Tuple[str,str]], is_unique: bool = False) -> None:
+		raise NotImplementedError()
+
+
+	def close(self) -> None:
+		self.connection.close()
---
+++ model/bhamon_orchestra_model/database/sql_database_model.py
@@ -0,0 +1,111 @@
+from sqlalchemy.schema import MetaData, Table, Column
+from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
+from sqlalchemy.types import Boolean, String, JSON
+
+
+metadata = MetaData()
+
+project = Table("project", metadata,
+	Column("identifier", String, nullable = False),
+	Column("display_name", String, nullable = False),
+	Column("services", JSON, nullable = False),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("identifier"),
+)
+
+job = Table("job", metadata,
+	Column("project", String, nullable = False),
+	Column("identifier", String, nullable = False),
+	Column("display_name", String, nullable = False),
+	Column("description", String, nullable = False),
+	Column("workspace", String, nullable = False),
+	Column("steps", JSON, nullable = False),
+	Column("parameters", JSON, nullable = False),
+	Column("properties", JSON, nullable = False),
+	Column("is_enabled", Boolean, nullable = False),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("project", "identifier"),
+	ForeignKeyConstraint([ "project" ], [ "project.identifier" ]),
+)
+
+run = Table("run", metadata,
+	Column("project", String, nullable = False),
+	Column("identifier", String, nullable = False),
+	Column("job", String, nullable = False),
+	Column("parameters", JSON, nullable = False),
+	Column("source", JSON, nullable = False),
+	Column("worker", String, nullable = True),
+	Column("status", String, nullable = False),
+	Column("steps", JSON, nullable = True),
+	Column("start_date", String, nullable = True),
+	Column("completion_date", String, nullable = True),
+	Column("results", JSON, nullable = True),
+	Column("should_cancel", Boolean, nullable = False),
+	Column("should_abort", Boolean, nullable = False),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("project", "identifier"),
+	ForeignKeyConstraint([ "project" ], [ "project.identifier" ]),
+	# No ForeignKeyConstraint on job, since it can be deleted
+	# No ForeignKeyConstraint on worker, since it can be deleted
+)
+
+schedule = Table("schedule", metadata,
+	Column("project", String, nullable = False),
+	Column("identifier", String, nullable = False),
+	Column("display_name", String, nullable = False),
+	Column("job", String, nullable = False),
+	Column("parameters", JSON, nullable = False),
+	Column("expression", String, nullable = False),
+	Column("is_enabled", Boolean, nullable = False),
+	Column("last_run", String, nullable = True),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("project", "identifier"),
+	ForeignKeyConstraint([ "project" ], [ "project.identifier" ]),
+	ForeignKeyConstraint([ "project", "job" ], [ "job.project", "job.identifier" ]),
+	ForeignKeyConstraint([ "project", "last_run" ], [ "run.project", "run.identifier" ]),
+)
+
+user = Table("user", metadata,
+	Column("identifier", String, nullable = False),
+	Column("display_name", String, nullable = False),
+	Column("roles", JSON, nullable = False),
+	Column("is_enabled", Boolean, nullable = False),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("identifier"),
+)
+
+user_authentication = Table("user_authentication", metadata,
+	Column("identifier", String, nullable = False),
+	Column("user", String, nullable = False),
+	Column("type", String, nullable = False),
+	Column("description", String, nullable = True),
+	Column("hash_function", String, nullable = True),
+	Column("hash_function_parameters", JSON, nullable = True),
+	Column("hash_function_salt", String, nullable = True),
+	Column("secret", String, nullable = True),
+	Column("expiration_date", String, nullable = True),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("user", "identifier"),
+	ForeignKeyConstraint([ "user" ], [ "user.identifier" ]),
+)
+
+worker = Table("worker", metadata,
+	Column("identifier", String, nullable = False),
+	Column("owner", String, nullable = False),
+	Column("version", String, nullable = False),
+	Column("display_name", String, nullable = False),
+	Column("properties", JSON, nullable = False),
+	Column("is_enabled", Boolean, nullable = False),
+	Column("is_active", Boolean, nullable = False),
+	Column("should_disconnect", Boolean, nullable = False),
+	Column("creation_date", String, nullable = False),
+	Column("update_date", String, nullable = False),
+	PrimaryKeyConstraint("identifier"),
+	ForeignKeyConstraint([ "owner" ], [ "user.identifier" ]),
+)
