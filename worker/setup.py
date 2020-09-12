d3ae26afd222c0734f455595065220f37465e1ae
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-09-12 21:53:02 +0200

    Upgrade requests dependency to 2.24

--- worker/setup.py
+++ worker/setup.py
@@ -24,7 +24,7 @@ parameters.update({
 	],
 
 	"python_requires": "~= 3.7",
-	"install_requires": [ "filelock ~= 3.0", "requests ~= 2.23", "websockets ~= 7.0" ],
+	"install_requires": [ "filelock ~= 3.0", "requests ~= 2.24", "websockets ~= 7.0" ],
 })
 
 setuptools.setup(**parameters)
