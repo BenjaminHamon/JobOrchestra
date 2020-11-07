022445f28450f68d22430a2ad32e69c344946717
Benjamin Hamon <hamon.benjamin@gmail.com>
2020-11-07 22:15:42 +0100

    Add directory filter option for test since pytest filter argument cannot do it

--- development/commands/test.py
+++ development/commands/test.py
@@ -13,12 +13,16 @@ def configure_argument_parser(environment, configuration, subparsers): # pylint:
 	parser = subparsers.add_parser("test", help = "run the test suite")
 	parser.add_argument("--identifier", default = str(uuid.uuid4()), metavar = "<identifier>", help = "specify a identifier for the run (default to a GUID)")
 	parser.add_argument("--filter", metavar = "<expression>", help = "specify a string expression to select tests to run")
+	parser.add_argument("--filter-directory", default = ".", metavar = "<path>", help = "specify a directory to select tests to run")
 	parser.set_defaults(func = run)
 
 
 def run(environment, configuration, arguments): # pylint: disable = unused-argument
+	python_executable = environment["python3_executable"]
 	result_directory = os.path.join(configuration["artifact_directory"], "test_results")
-	report = python_test.run_pytest(environment["python3_executable"], result_directory, arguments.identifier, "./test",  arguments.filter, simulate = arguments.simulate)
+	test_directory = os.path.normpath(os.path.join("test", arguments.filter_directory))
+
+	report = python_test.run_pytest(python_executable, result_directory, arguments.identifier, test_directory, arguments.filter, simulate = arguments.simulate)
 
 	if arguments.results:
 		save_results(arguments.results, report, simulate = arguments.simulate)
